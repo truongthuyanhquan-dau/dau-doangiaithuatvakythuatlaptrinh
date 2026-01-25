f36b0bc961e681924feafa5c6b3b0ddddabfa1ac
truongthuyanhquan-dau <truongthuyanhquan2209@gmail.com>
2026-01-25 19:10:33 +0700

    Add files via upload

---
+++ Ransom Note.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+    
+        
+        if len(ransomNote) > len(magazine):
+            return False
+        
+        char_count = {}
+        for char in magazine:
+            char_count[char] = char_count.get(char, 0) + 1
+            
+        for char in ransomNote:
+            
+            if char_count.get(char, 0) <= 0:
+                return False
+            
+            char_count[char] -= 1
+            
+        return True
+        
